35a4056c5c2076c5790c30928aa1af3ebebc962c
EVGENIY SILCHENKO <eve.ashumi@gmail.com>
2016-11-25 02:23:12 +0300

    Добавлено: Процедура с перегрузкой функции для нового объекта

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ public:
     Base() { cout << "+ Собрать объект Base\n"; }
     ~Base() { cout << "+ Разобрать объект Base\n"; }
 
-    void func() { cout << "+ Функция func() класса Base\n"; }
+    void func() const { cout << "+ Функция func() класса Base\n"; }
 };
 
 class Child: public Base
@@ -17,23 +17,30 @@ public:
     Child() { cout << "+ Собрать объект Child\n"; }
     ~Child() { cout << "+ Разобрать объект Child\n"; }
 
-    void func() { cout << "+ Функция func() класса Child\n"; }
+    void func() const { cout << "+ Функция func() класса Child\n"; }
 };
+
+void Function(const Base &obj)
+{
+    cout << "+ Вызов метода func() для объекта obj" << endl;
+    obj.func();
+}
 
 int main(int argc, char *argv[])
 {
     cout << "+ Создать объект Base" << endl;
     Base base;
+
     cout << "+ Создать объект Child" << endl;
     Child child;
 
     cout << "+ Вызов метода func() для объекта Base" << endl;
-    base.func();
+    Function(base);
 
     cout << "+ Вызов метода func() для объекта Child" << endl;
-    child.func();
+    Function(child);
 
-    cout << "* Добавление перегрузки функции для наследуемого класса" << endl;
+    cout << "* Добавление процедуры с перегрузкой функции для нового объекта" << endl;
 
     return 0;
 }
